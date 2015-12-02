918877da2c3a3352e3405b58e9fc4cb695ccde84
Jan Hudecek <jhudecek17@gmail.com>
2015-12-01 19:15:38 -0600

    Sending message changed

--- app/src/main/java/com/example/gingerin/smstrysend/SendSmsActivity.java
+++ app/src/main/java/com/example/gingerin/smstrysend/SendSmsActivity.java
@@ -11,9 +11,14 @@ import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 
 public class SendSmsActivity extends AppCompatActivity {
 
@@ -137,14 +142,14 @@ public class SendSmsActivity extends AppCompatActivity {
         String unEncrypted = smsMessageET.getText().toString();
         String recipient = toPhoneNumberET.getText().toString();
 
-        PublicKey key = r.getRecipientKey(recipient);
+        PublicKey key = r.getRecipientKey(recipient + ".key");
         if(key == null) {
             Toast.makeText(this, "No public key specified for number " + recipient + ". Add key in Key Management.", Toast.LENGTH_LONG).show();
         } else {
             byte[] encByte = r.encrypt(unEncrypted, key);
             try {
                 String encrypted = new String(encByte, "ISO-8859-1");
-                EditText encTextEdit = (EditText) findViewById(R.id.encryptedText);
+                TextView encTextEdit = (TextView) findViewById(R.id.encryptedText);
                 encTextEdit.setText(encrypted);
             } catch (Exception e) {
                 e.printStackTrace();
